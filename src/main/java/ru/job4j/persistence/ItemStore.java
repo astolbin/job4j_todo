1175d1d22816d515c238618fcd750dc18f3e51fd
stolbin <stolbin@samsonpost.ru>
2022-06-03 23:00:30 +0300

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/persistence/ItemStore.java
+++ src/main/java/ru/job4j/persistence/ItemStore.java
@@ -8,112 +8,74 @@ import org.springframework.stereotype.Repository;
 import ru.job4j.model.Item;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Repository
 public class ItemStore {
     private static final Logger LOG = LoggerFactory.getLogger(ItemStore.class.getName());
-
     private final SessionFactory sf;
 
     public ItemStore(SessionFactory sf) {
         this.sf = sf;
     }
 
-    private List<Item> getByFilter(String filter) {
-        final Session session = sf.openSession();
-        List result = null;
-        try (session) {
-            session.beginTransaction();
-            result = session.createQuery("from ru.job4j.model.Item" + filter).list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.error("Exception", e);
-        }
-        return result;
-    }
-
     public List<Item> getAll() {
-        return getByFilter("");
+        return runWithSession(session -> session.createQuery("from Item").list());
     }
 
     public List<Item> getNew() {
-        return getByFilter(" where done = false");
+        return runWithSession(
+                session -> session.createQuery("from Item i where i.done = false").list()
+        );
     }
 
     public List<Item> getCompleted() {
-        return getByFilter(" where done = true");
+        return runWithSession(
+                session -> session.createQuery("from Item i where i.done = true").list()
+        );
     }
 
     public Item getById(int id) {
-        final Session session = sf.openSession();
-        Item result = null;
-        try (session) {
-            session.beginTransaction();
-            result = session.get(Item.class, id);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.error("Exception", e);
-        }
-        return result;
+        return runWithSession(session -> session.get(Item.class, id));
     }
 
     public void updateDescription(int id, String description) {
-        final Session session = sf.openSession();
-        try (session) {
-            session.beginTransaction();
-            String sql = "update Item i set i.description = :description where i.id = :id";
-            session.createQuery(sql)
-                    .setParameter("description", description)
-                    .setParameter("id", id)
-                    .executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.error("Exception", e);
-        }
+        String sql = "update Item i set i.description = :description where i.id = :id";
+        runWithSession(session -> session.createQuery(sql)
+                .setParameter("description", description)
+                .setParameter("id", id)
+                .executeUpdate());
     }
 
     public void updateDone(int id, boolean done) {
-        final Session session = sf.openSession();
-        try (session) {
-            session.beginTransaction();
-            String sql = "update Item i set i.done = :done where i.id = :id";
-            session.createQuery(sql)
-                    .setParameter("done", done)
-                    .setParameter("id", id)
-                    .executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.error("Exception", e);
-        }
+        String sql = "update Item i set i.done = :done where i.id = :id";
+        runWithSession(session -> session.createQuery(sql)
+                .setParameter("done", done)
+                .setParameter("id", id)
+                .executeUpdate());
     }
 
     public void add(Item item) {
-        final Session session = sf.openSession();
-        try (session) {
-            session.beginTransaction();
-            session.save(item);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.error("Exception", e);
-        }
+        runWithSession(session -> session.save(item));
     }
 
     public void delete(int id) {
+        runWithSession(session -> session.createQuery("delete from Item i WHERE i.id = :id")
+                .setParameter("id", id)
+                .executeUpdate());
+    }
+
+    private <T> T runWithSession(final Function<Session, T> command) {
         final Session session = sf.openSession();
+        T rsl = null;
         try (session) {
             session.beginTransaction();
-            Item item = new Item();
-            item.setId(id);
-            session.delete(item);
+            rsl = command.apply(session);
             session.getTransaction().commit();
         } catch (Exception e) {
             session.getTransaction().rollback();
             LOG.error("Exception", e);
         }
+        return rsl;
     }
 }
